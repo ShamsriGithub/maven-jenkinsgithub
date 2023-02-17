541dc822bf328e5e9cded3fe9ecdd8b7e47d4daa
shambhavee <shambhaveesrivastava@gmail.com>
2023-02-17 14:04:25 +0530

    first commit

---
+++ src/main/java/jenkinlab1/diplayexam.java
@@ -0,0 +1,20 @@
+package jenkinlab1;
+
+public class diplayexam {
+	public void show()
+	{
+		System.out.println("hello welcome to jenkins!");
+	}
+	public void showname()
+	{
+		System.out.println("hello shambhavee");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     diplayexam obj=new diplayexam();
+     obj.show();
+     obj.showname();
+	}
+
+}
